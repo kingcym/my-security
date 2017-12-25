5e3b54ca1ae529f7ccf023aaead23ce3403e2dc5
caiyaming <caiyaming@winkind-tech.com>
2017-12-26 01:06:25 +0800

    添加pom,各种配置

---
+++ cym-security-app/src/main/java/com/cym/security/CymSecurityAppApplication.java
@@ -0,0 +1,12 @@
+package com.cym.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CymSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CymSecurityAppApplication.class, args);
+	}
+}
