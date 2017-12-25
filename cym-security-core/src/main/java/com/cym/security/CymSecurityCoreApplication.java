a2a99c50cb1976d142fba5a4ac38c2371edef4fb
caiyaming <caiyaming@winkind-tech.com>
2017-12-25 23:35:58 +0800

    no message

---
+++ cym-security-core/src/main/java/com/cym/security/CymSecurityCoreApplication.java
@@ -0,0 +1,12 @@
+package com.cym.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CymSecurityCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CymSecurityCoreApplication.class, args);
+	}
+}
