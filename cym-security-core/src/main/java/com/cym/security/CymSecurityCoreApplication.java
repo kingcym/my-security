043f890155c2f6d5d43537fe8b3e5114dfb78d6f
caiyaming <caiyaming@winkind-tech.com>
2017-12-26 11:35:22 +0800

    no message

--- cym-security-core/src/main/java/com/cym/security/CymSecurityCoreApplication.java
+++ cym-security-core/src/main/java/com/cym/security/CymSecurityCoreApplication.java
@@ -2,6 +2,8 @@ package com.cym.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CymSecurityCoreApplication {
@@ -9,4 +11,7 @@ public class CymSecurityCoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CymSecurityCoreApplication.class, args);
 	}
+
+
+
 }
--- cym-security-demo/src/main/java/com/cym/security/CymSecurityDemoApplication.java
+++ cym-security-demo/src/main/java/com/cym/security/CymSecurityDemoApplication.java
@@ -1,12 +1,23 @@
 package com.cym.security;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+
 public class CymSecurityDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CymSecurityDemoApplication.class, args);
 	}
+
+	@GetMapping("/hello")
+	public String hello (){
+		return "hello world====";
+	}
+
 }
